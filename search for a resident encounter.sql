bcc8a70d4b3578e1dee741cf9375a1edafa9e5f8
Mohamed Eldeeb <modeeb@gmail.com>
2024-07-25 17:42:40 -0300

    Create search for a resident encounter

---
+++ search for a resident encounter.sql
@@ -0,0 +1,7 @@
+select * from code_value where display like 'Resident';
+
+select * from encounter e
+where e.encntr_type_cd = 49848542 
+and e.depart_dt_tm is null
+and e.location_cd = 46686151
+and rownum <= 10;
